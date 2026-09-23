43e5eb1a2c9dd3df8f858545b217058680d31b2c
Mangotimmy <73014565+Mangotimmy@users.noreply.github.com>
2026-09-24 04:59:07 +0800

    Update script name and description for clarity

--- ilearning.js
+++ ilearning.js
@@ -1,8 +1,8 @@
 // ==UserScript==
-// @name         中原 cycu iLearning 2.0 頁面addon (優化修復版)
+// @name         中原 cycu iLearning 2.0 頁面addon
 // @namespace    http://ilearning.cycu.edu.tw/
 // @version      2.1.4
-// @description  可任意拖曳漂浮面板（支援 PC 滑鼠/iPad 觸控與座標防出界記憶）、HTML5 Video 原生全螢幕、影片進度條、獨立影片/PDF直載、大綱抽屜解鎖、全格式教材自訂打包 ZIP，並支援 Web Audio 600% 爆音引擎與 PDF 護眼深色模式。
+// @description  可任意拖曳漂浮面板（支援 PC 滑鼠/iPad 觸控與座標防出界記憶）、HTML5 Video 原生全螢幕、影片進度條、獨立影片/PDF直載、大綱抽屜解鎖、全格式教材自訂打包 ZIP，並支援 Web Audio 600% 與 PDF 護眼深色模式。
 // @author       Mangotimmy
 // @license      MIT
 // @match        *://ilearning.cycu.edu.tw/*
